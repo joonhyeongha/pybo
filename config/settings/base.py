ecc31f98914b5ef4dabd4c5fe8003871619e7179
전형하 <joonhyeongha@naver.com>
2022-12-13 16:51:49 +0900

    settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a^8f2yvzobifvrxo*ho58fh%+zc&)h84rh_)7k2ku+a_0r3m@d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['146.56.104.70']
 
 
 # Application definition
--- website.cmd
+++ website.cmd
@@ -1,3 +1,4 @@
 @echo off
 cd C:/Users/user/PycharmProjects/website/
+set DJANGO_SETTINGS_MODULE=config.settings.local
 C:./venv/website/scripts/activate
