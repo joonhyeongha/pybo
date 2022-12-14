b51a3a1036a892c9e281f578f64607d577f9ea18
전형하 <joonhyeongha@naver.com>
2022-12-14 17:00:33 +0900

    tables markdown 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -11,5 +11,5 @@ def sub(value, arg):
 
 @register.filter
 def mark(value):
-    extensions = ["nl2br", "fenced_code"]
+    extensions = ["nl2br", "fenced_code", "tables"]
     return mark_safe(markdown.markdown(value, extensions=extensions))
